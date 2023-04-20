141d975b31b36615cd86f18b33a2a5804e99dcbd
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 19:19:36 -0500

    Fibonacci Loop Created

---
+++ FibonacciLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FibonacciLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a natural number: ");
+            int numberInput = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.WriteLine("{0} {1}", a, b);
+
+            for (int i = 2; i< numberInput; i++)
+            {
+                c = a + b;
+                if (c > numberInput)
+                    break;
+
+                Console.Write(" {0}", c);
+
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
