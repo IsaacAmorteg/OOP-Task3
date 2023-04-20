048277dd3634974ed81c0756f36c73e5015dbac9
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 19:26:36 -0500

    Initialization of Fibonacci with Recurssion

---
+++ FibonacciRecurssion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciRecurssion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a natural number: ");
+            int numberInput = int.Parse(Console.ReadLine());
+
+           Console.Write("Test");
+
+            //for (int i = 2; i < numberInput; i++)
+            //{
+            //    c = a + b;
+             //   if (c > numberInput)
+               //     break;
+
+                //Console.Write(" {0}", c);
+
+                //a = b;
+                //b = c;
+
+           // }
+        }
+    }
+}
